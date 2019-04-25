4034c71f3da33a55743bc9d262219adfad8e89c4
JasJohn3 <33557960+JasJohn3@users.noreply.github.com>
2019-04-24 21:53:28 -0700

    Astar and Dijkstra paths created.

--- Graphs.py
+++ Graphs.py
@@ -3,14 +3,14 @@ import networkx as nx
 import random
 #Declaring our Graph class object
 Graph = nx.grid_graph(dim=[12,12])
-
-for i in range(50):
-    Graph.add_node(i)
-    if i >= 1:
-        Graph.add_edge(i, i-1)
-
-for i in range(50):
-    Graph.add_edge(i,random.randint(1,50))
+Random_Graph = nx.Graph()
+# for i in range(50):
+#     Graph.add_node(i)
+#     if i >= 1:
+#         Graph.add_edge(i, i-1)
+#
+# for i in range(50):
+#     Graph.add_edge(i,random.randint(1,50))
 
 
 pos = nx.spring_layout(Graph)  # positions for all nodes
@@ -29,16 +29,35 @@ def distance(a, b):
     (x2, y2) = b
     return ((x1-x2)**2 + (y1-y2)**2)**0.5
 astar_path = nx.astar_path(Graph,(0,0),(random.randint(1,10),random.randint(1, 10)),distance)
+print("Generating AStar Path")
 print(astar_path)
-xlist = []
-ylist = []
-#cant access astar path information
-# for i in astar_path:
-#     for x in astar_path[i]:
-#         xlist.append(astar_path[i][x])
-#         print(xlist)
+dijkstra_path = nx.dijkstra_path(Graph,(0,0),(random.randint(1,10),random.randint(1, 10)))
+print("Generating dijkstra path")
+print(dijkstra_path)
+
+plt.axis('off')
+plt.show()
 
 
-plt.scatter(xlist,ylist, color = 'Red')
+Random_Graph = nx.Graph()
+for i in range(50):
+    Random_Graph.add_node(i)
+    if i >= 1:
+        Random_Graph.add_edge(i, i-1)
+
+for i in range(50):
+    Random_Graph.add_edge(i,random.randint(1,50))
+
+
+pos = nx.spring_layout(Random_Graph)  # positions for all nodes
+# nodes
+nx.draw_networkx_nodes(Random_Graph, pos, node_size=100)
+
+# edges
+nx.draw_networkx_edges(Random_Graph, pos, width=2)
+nx.draw_networkx_edges(Random_Graph, pos, width=2, alpha=0.5, edge_color='cyan', style='dashed')
+
+# labels
+nx.draw_networkx_labels(Random_Graph, pos, font_size=11, font_family='sans-serif')
 plt.axis('off')
 plt.show()
